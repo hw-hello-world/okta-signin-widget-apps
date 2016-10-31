1fa1c11e1959429cfc87eef4021700a70382a600
Haisheng.Wu.Okta <haisheng.wu@okta.com>
2016-10-31 10:42:37 -0700

    remove useless code.

--- jquery-okta-idp/js/main.js
+++ jquery-okta-idp/js/main.js
@@ -3,49 +3,6 @@
 /* global redirectUrl */
 /* global $ */
 
-(function () {
-
-  var okta = window.okta;
-
-  var headers = {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
-    'X-Okta-SDK': 'hw-hello-world-jquery',
-    'X-Okta-User-Agent-Extended': 'hw-hello-world-jquery'
-  };
-
-  function ajax(method, url, data) {
-    return $.ajax({
-      url: okta.baseUrl + url,
-      method: method,
-      headers: headers,
-      data: data || {},
-      xhrFields: {
-        // <<<
-        // https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/withCredentials
-        // <<<
-        withCredentials: true
-      }
-    });
-  }
-
-
-  var api = {
-    ajax: ajax,
-    user: {
-      me: function () {
-        return ajax('GET', '/api/v1/users/me');
-      },
-      tabs: function () {
-        return ajax('GET', '/api/v1/users/me/home/tabs');
-      }
-    }
-  };
-
-  window.okta.api = api;
-
-})();
-
 $(function () {
 
   const LS = localStorage;
