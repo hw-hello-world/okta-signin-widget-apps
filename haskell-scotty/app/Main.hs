82a44ece6561c8632654c301a17b8b984a668c3a
Haisheng.Wu.Okta <haisheng.wu@okta.com>
2016-11-11 16:36:52 -0800

    wip - haskell - blocked with parse claims encoding

--- haskell-scotty/app/Main.hs
+++ haskell-scotty/app/Main.hs
@@ -4,11 +4,26 @@
 
 module Main where
 
-import Data.Aeson (decode)
+import Data.List
+import qualified Data.ByteString.Base64.Lazy as B64
+import Network.HTTP.Types.Status
+import Data.ByteString.Lazy (ByteString)
+import Data.Monoid (mempty)
+import Control.Arrow (second)
+import Control.Monad.IO.Class (liftIO)
+import Data.Aeson (decode, FromJSON, parseJSON)
+import Data.Aeson.Types
 import           Data.Aeson.TH                 (defaultOptions, deriveJSON)
 
+import Network.HTTP.Types.Header
+import Network.HTTP.Simple
+--import Network.HTTP.Client
 import qualified Data.ByteString.Lazy.Char8 as BS
+--import qualified Data.ByteString.Char8 as BS
+
 import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
 import Web.Scotty
 import Text.Blaze.Html5 ((!), label)
 import qualified Text.Blaze.Html5 as H
@@ -31,6 +46,51 @@ data Config = Config { baseUrl :: String
                      } deriving (Show)
 
 $(deriveJSON defaultOptions ''Config)
+
+
+data Claims = Claims { sub :: String
+                     , email :: String
+                     , ver :: Int
+                     , iss :: String
+                     , aud :: String
+                     , iat :: String
+                     , exp :: Int
+                     , jti :: String
+                     , amr :: String
+                     , idp :: String
+                     , nonce :: String
+                     --, auth_time :: Int
+                     --, at_hash :: String
+                     } deriving (Show)
+$(deriveJSON defaultOptions ''Claims)
+
+data TokenResponse = TokenResponse { accessToken :: String
+                                   , tokenType :: String
+                                   , expiresIn :: Int
+                                   , scope :: String
+                                   , idToken :: String
+                                   } deriving (Show)
+
+instance FromJSON TokenResponse where
+    parseJSON (Object v) = TokenResponse
+                           <$> v .: "access_token"
+                           <*> v .: "token_type"
+                           <*> v .: "expires_in"
+                           <*> v .: "scope"
+                           <*> v .: "id_token"
+    parseJSON _          = mempty
+
+
+data ErrorResponse = ErrorResponse { errorCode :: String
+                                   , errorDescription :: String
+                                   } deriving (Show)
+
+instance FromJSON ErrorResponse where
+    parseJSON (Object v) = ErrorResponse
+                           <$> v .: "error"
+                           <*> v .: "error_description"
+    parseJSON _          = mempty
+
 
 -- FIXME: include config.json in build result
 --
@@ -58,10 +118,58 @@ app c = scotty 12234 $ do
   get "/login-okta" $ html . renderHtml $ loginOktaPage c
   get "/logout" $ text "FIXME: logout"
   get "/callback" $ do
+    -- FIXME: code may not exists when the request error and it will contain
+    --        error=access_denied&error_description=User+is+not+assigned+to+the+client+application
+    --
     code <- param "code"
     state <- param "state"
+    _ <- liftIO (fetchToken c code state)
     html . renderHtml $ callbackPage code state
 
+
+
+--
+-- 1. [ ] read cookie and verify state
+-- 2. [ ] fetch token
+-- 3. [ ] error handling
+-- 4. [ ] verify nonce from cookie
+-- 5. [ ] set cookie and redirect to home page
+--
+fetchToken :: Config -> Text -> Text -> IO ()
+fetchToken c code state = do
+  req <- parseRequest (baseUrl c ++ "/oauth2/v1/token")
+  resp <- httpLbs (updateReq req)
+  let rawBody = getResponseBody resp
+  let rStatus = getResponseStatus resp
+  if rStatus == status200 then do
+    t <- return (decode rawBody :: Maybe TokenResponse)
+    case t of
+      Just tokenResp -> do
+        let idToken' = idToken tokenResp
+        let claimsCode = dropWhile (== '.') idToken'
+        -- FIXME: got "invalid padding" with decode but decodeLenient is fine but its result cant be decode to JSON type
+        let claims = B64.decodeLenient (BS.pack claimsCode)
+        print claims
+        print (decode claims :: Maybe Claims)
+        print tokenResp
+      Nothing -> putStrLn "Cant parse success response"
+  else if rStatus == status401 then print (decode rawBody :: Maybe ErrorResponse)
+  else print rawBody
+  print rawBody
+  where updateReq = setM . setH . setQ
+        setM = setRequestMethod "POST"
+        setH = setRequestHeaders [ (hAuthorization, BS.toStrict (BS.pack "Basic: " `BS.append` B64.encode (BS.pack $ clientId c ++ ":" ++ clientSecret c)))
+                                 , (hContentType, "application/x-www-form-urlencoded")
+                                 ]
+        setQ = setRequestQueryString $ map (second Just) [ ("grant_type", "authorization_code")
+                                                         , ("code", T.encodeUtf8 code)
+                                                         , ("redirect_uri", BS.toStrict (BS.pack (redirectUri c)))
+                                                         ]
+
+
+------------------------------
+-- Stack Files
+------------------------------
 
 oktaSignInBase :: String
 oktaSignInBase = "https://ok1static.oktacdn.com/assets/js/sdk/okta-signin-widget/1.8.0"
@@ -76,6 +184,10 @@ oktaSignInJs, oktaSignInCss, oktaSignInTheme :: H.AttributeValue
 oktaSignInJs = fromString (oktaSignInBase ++ "/js/okta-sign-in.min.js")
 oktaSignInCss = fromString $ oktaSignInBase ++ "/css/okta-sign-in.min.css"
 oktaSignInTheme = fromString $ oktaSignInBase ++ "/css/okta-theme.css"
+
+------------------------------
+-- HTML
+------------------------------
 
 homePage :: H.Html
 homePage = H.html $ do
@@ -96,7 +208,6 @@ loginOktaPage c = H.html $ do
     css "/css/main.css"
   H.h1 "Okta Login!"
   H.p $ H.a ! H.href "#" ! H.id "loginWithOkta" $ "Login with Okta"
-  -- H.div ! H.id "okta-sign-in-container" $ ""
   H.p $ H.a ! H.href "/" $ "Back to home"
   H.script ! H.type_ "text/javascript" $ H.toHtml $
     unlines [ "var baseUrl = '" ++ baseUrl c ++ "';"
--- haskell-scotty/src/Lib.hs
+++ haskell-scotty/src/Lib.hs
@@ -1,6 +1,71 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+module Lib where
+
+import Data.ByteString.Lazy (ByteString)
+
+import Data.Aeson (decode, FromJSON, parseJSON)
+import Data.Aeson.Types
+import           Data.Aeson.TH                 (defaultOptions, deriveJSON)
+import qualified Data.ByteString.Base64.Lazy as B64
+import qualified Data.ByteString.Lazy.Char8 as BS
+
+data TokenResponse = TokenResponse { accessToken :: String
+                                   , tokenType :: String
+                                   , expiresIn :: Int
+                                   , scope :: String
+                                   , idToken :: String
+                                   } deriving (Show)
+
+instance FromJSON TokenResponse where
+    parseJSON (Object v) = TokenResponse
+                           <$> v .: "access_token"
+                           <*> v .: "token_type"
+                           <*> v .: "expires_in"
+                           <*> v .: "scope"
+                           <*> v .: "id_token"
+    parseJSON _          = mempty
+
+
+data ErrorResponse = ErrorResponse { errorCode :: String
+                                   , errorDescription :: String
+                                   } deriving (Show)
+
+instance FromJSON ErrorResponse where
+    parseJSON (Object v) = ErrorResponse
+                           <$> v .: "error"
+                           <*> v .: "error_description"
+    parseJSON _          = mempty
+
+
+data Claims = Claims { sub :: String
+                     , email :: String
+                     , ver :: Int
+                     , iss :: String
+                     , aud :: String
+                     , iat :: String
+                     , exp :: Int
+                     , jti :: String
+                     , amr :: String
+                     , idp :: String
+                     , nonce :: String
+                     --, auth_time :: Int
+                     --, at_hash :: String
+                     } deriving (Show)
+$(deriveJSON defaultOptions ''Claims)
+
+
+parseSuccessResponse :: ByteString -> IO (Either String Claims)
+parseSuccessResponse respBody = case decode respBody of
+  Just tokenResp -> let idToken' = idToken tokenResp
+                        claimsCode = tail (dropWhile (/= '.') idToken')
+                    in
+                      do
+                      print idToken'
+                      print claimsCode
+                      print $ B64.decodeLenient (BS.pack claimsCode)
+                      case decode $ B64.decodeLenient (BS.pack claimsCode) of
+                        Just c -> return $ Right c
+                        Nothing -> return $ Left "Cant parse claims from response body"
+  Nothing -> return $ Left "Cant parse response body"
